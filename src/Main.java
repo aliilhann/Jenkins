c2c40c556e5a3d50cb1b2c1e18f38812c7ec05c0
aliilhann <alirizailhan89@gmail.com>
2023-03-26 14:54:28 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for(int i=0 ; i<10; i++) {
-            System.out.println("Hello world");
+        for(int i=0 ; i<12; i++) {
+            System.out.println("Hello new world");
             Thread.sleep(4000);
         }
     }
